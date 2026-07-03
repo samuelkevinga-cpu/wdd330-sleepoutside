77b16aefeb96a05304b3750659265a898c463fa4
Tasha-Vidal <tvidalbravo@byupathway.edu>
2026-07-03 17:21:24 -0500

    corrected cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-  cart.push(product);
-  localStorage.setItem("so-cart", JSON.stringify(cart));
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
