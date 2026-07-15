76d0ca9d34c31b90f2e5506e1c5e1417a971d86d
sebasss18 <sebasbernals11@gmail.com>
2026-07-15 08:23:14 -0700

    Team Assignment W03

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { updateCartCount } from "./utils.mjs";
+import { updateCartCount, loadHeaderFooter } from "./utils.mjs";
 
 
 const dataSource = new ProductData("tents");
@@ -8,4 +8,5 @@ const listElement = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, listElement);
 productList.init();
 
+loadHeaderFooter();
 updateCartCount();
